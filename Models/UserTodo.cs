cf9b2291fb5449162eabe0af1c0ef3a8f5d28057
Minayev Aleksandr <tenshisasha@gmail.com>
2021-01-14 20:43:51 +0300

    Install Identity, EFCore.SQLServer, EFCore.Tools packeges

---
+++ Models/UserTodo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustDo_Web.Models
+{
+    public class UserTodo: IdentityUser
+    {
+
+    }
+}
